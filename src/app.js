9614285031c9c891d51ab316c1c2b80ebf713da7
nkurunziza1 <Alexendre.Nkurunziza@gmail.com>
2023-05-01 14:05:29 -0900

    chore:(ch-config-eslint-185069132): config eslint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
-const app=express()
-app.use(express.json())
-app.use(cors())
-mongoose.connect(process.env.DEV_DATABASE)
+const app=express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.DEV_DATABASE);
 
-export default app
+export default app;
