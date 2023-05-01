d08e8118f5f5bb9fee3b212c7af51229f6bb299e
buka1calvin <calvinbukarani@gmail.com>
2023-05-01 02:30:59 +0200

    feat(google-auth-185068610):installing packages

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const app=express()
+app.use(cors({origin:"*",methods:["GET","POST","DELETE","UPDATE","PUT","PATCH"]}))
+mongoose.connect(process.env.DEV_DATABASE,{ useNewUrlParser: true })
+.then(() => {
+    app.use(express.json())
+})	
+
+export default app
