5b1dde3374da1d7dc0ec33a5c3ad9f92ede2a934
buka1calvin <calvinbukarani@gmail.com>
2023-05-01 02:30:59 +0200

    feat(google-auth-185068610):installing packages

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import app from './app'
+import 'dotenv/config'
+
+
+const port=process.env.PORT 
+app.listen(port,()=>{
+    console.log(`server is running at server http://localhost:${port}`)
+})
+
